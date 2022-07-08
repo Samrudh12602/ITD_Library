4af8641974f8c284197230e34be645b5a271b937
Samrudh12602 <samdhaimodkar@gmail.com>
2022-07-08 20:21:30 +0530

    samrudh

--- app/src/main/java/com/example/loginpage/YearSelectionActivity.java
+++ app/src/main/java/com/example/loginpage/YearSelectionActivity.java
@@ -1,15 +1,7 @@
 package com.example.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 public class YearSelectionActivity extends AppCompatActivity {
     @Override
