2d6d8e1c99abd4b7ac75207d1affd6515437d801
srujanaa6 <srujanaparesh@gmail.com>
2022-07-08 19:50:26 +0530

    srujana

---
+++ app/src/main/java/com/example/loginpage/BE_subjects.java
@@ -0,0 +1,14 @@
+package com.example.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BE_subjects extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_be_subjects);
+    }
+}
