68010b479da7feca4a78bf0a1b11d32f099ec98e
srujanaa6 <srujanaparesh@gmail.com>
2022-07-09 18:37:31 +0530

    srujana

--- app/src/main/java/com/example/loginpage/BE_subjects.java
+++ app/src/main/java/com/example/loginpage/BE_subjects.java
@@ -2,14 +2,26 @@ package com.example.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BE_subjects extends AppCompatActivity {
-
+Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_be_subjects);
+        back=findViewById(R.id.backbutton);
+        View.OnClickListener listener= new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),YearSelectionActivity.class));
+
+            }
+        };
+        back.setOnClickListener(listener);
     }
 }
--- app/src/main/java/com/example/loginpage/TE_subjects.java
+++ app/src/main/java/com/example/loginpage/TE_subjects.java
@@ -2,15 +2,28 @@ package com.example.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class TE_subjects extends AppCompatActivity {
-
+Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_te_subjects);
+        back=findViewById(R.id.backbutton);
+        View.OnClickListener listener= new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),YearSelectionActivity.class));
+
+            }
+        };
+        back.setOnClickListener(listener);
     }
+
 }
