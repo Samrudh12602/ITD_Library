370cc7ef335806885515fe55d95548186ab41020
Samrudh12602 <samdhaimodkar@gmail.com>
2022-07-09 23:13:25 +0530

    samrudh

--- app/src/main/java/com/example/loginpage/YearSelectionActivity.java
+++ app/src/main/java/com/example/loginpage/YearSelectionActivity.java
@@ -1,5 +1,6 @@
 package com.example.loginpage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
@@ -7,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -32,9 +34,7 @@ public class YearSelectionActivity extends AppCompatActivity {
 //        logout.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
-//                auth.signOut();
-//                startActivity(new Intent(YearSelectionActivity.this,MainActivity.class));
-//                finish();
+//
 //            }
 //        });
         View.OnClickListener listener= new View.OnClickListener() {
@@ -59,5 +59,14 @@ public class YearSelectionActivity extends AppCompatActivity {
         MenuInflater inflater= getMenuInflater();
         inflater.inflate(R.menu.menu,menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.about:startActivity(new Intent(YearSelectionActivity.this,AboutUsActivity.class));break;
+            case R.id.logout:auth.signOut();startActivity(new Intent(YearSelectionActivity.this,MainActivity.class));finish();break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
