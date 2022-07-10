30e66f261ef881d052e91c708245c377f69520f1
Samrudh12602 <samdhaimodkar@gmail.com>
2022-07-10 12:07:38 +0530

    samrudh

---
+++ app/src/main/java/com/example/loginpage/PdfAdapter.java
@@ -0,0 +1,50 @@
+package com.example.loginpage;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class PdfAdapter extends RecyclerView.Adapter<PdfAdapter.PdfViewHolder> {
+
+    private Context context;
+    private List<PdfData> list;
+
+    public PdfAdapter(Context context, List<PdfData> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public PdfViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.pdf_item_layout,parent,false);
+        return new PdfViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PdfViewHolder holder, int position) {
+        holder.pdfName.setText(list.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class PdfViewHolder extends RecyclerView.ViewHolder {
+        private TextView pdfName;
+        private Button download;
+
+        public PdfViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
