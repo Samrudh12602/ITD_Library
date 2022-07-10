fce904fdb8970530acc2a6b696ec0084d415e9d8
Samrudh12602 <samdhaimodkar@gmail.com>
2022-07-10 14:54:20 +0530

    samrudh

--- app/src/main/java/com/example/loginpage/PdfViewActivity.java
+++ app/src/main/java/com/example/loginpage/PdfViewActivity.java
@@ -1,16 +1,79 @@
 package com.example.loginpage;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.stream.Stream;
 
 public class PdfViewActivity extends AppCompatActivity {
-    String url;
+    private String url;
+    private PDFView pdfView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf_view);
         url=getIntent().getStringExtra("pdfUrl");
+        pdfView=findViewById(R.id.pdfView);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        new PdfDownload().execute(url);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater= getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.about:break;
+            case R.id.logout:break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private class PdfDownload extends AsyncTask<String,Void, InputStream> {
+
+        @Override
+        protected InputStream doInBackground(String... strings) {
+            InputStream inpurStream= null;
+            try {
+                URL url= new URL(strings[0]);
+                HttpURLConnection connection= (HttpURLConnection) url.openConnection();
+
+                if(connection.getResponseCode()==200){
+                    inpurStream=new BufferedInputStream(connection.getInputStream());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return inpurStream;
+        }
+
+        @Override
+        protected void onPostExecute(InputStream inputStream) {
+           pdfView.fromStream(inputStream).load();
+        }
     }
 }
